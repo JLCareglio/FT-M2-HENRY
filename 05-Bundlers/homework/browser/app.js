848830944ea2219b7320e00f4faa0c779204ecbd
Jesús Lautaro Careglio Albornoz <jlcareglioalbornoz@gmail.com>
2022-08-08 16:56:30 -0300

    ✅

--- 05-Bundlers/homework/browser/app.js
+++ 05-Bundlers/homework/browser/app.js
@@ -1,29 +1,34 @@
-(function () {
+// var whiteboard = window.whiteboard; // Obj EventEmitter (intancia) con sus metodos
+var whiteboard = require("./whiteboard");
 
-  var whiteboard = window.whiteboard;
-  var socket = window.io(window.location.origin);
+var io = require("socket.io-client");
+var socket = io(window.location.origin);
 
-  socket.on('connect', function () {
-    console.log('Connected!');
-  });
+// --> ES6
+// import {whiteboard} from './whiteboard'
+// import {io} from 'socket.io-client'
 
-  socket.on('load', function (strokes) {
 
-    strokes.forEach(function (stroke) {
-      var start = stroke.start;
-      var end = stroke.end;
-      var color = stroke.color;
-      whiteboard.draw(start, end, color, false);
-    });
 
-  });
+// var socket = window.io(window.location.origin); --> estamos importando socket IO --> lo invocamos pasandole location origin
 
-  socket.on('draw', function (start, end, color) {
+socket.on("connect", function () {
+  console.log("Connected!");
+});
+
+socket.on("load", function (strokes) {
+  strokes.forEach(function (stroke) {
+    var start = stroke.start;
+    var end = stroke.end;
+    var color = stroke.color;
     whiteboard.draw(start, end, color, false);
   });
+});
 
-  whiteboard.on('draw', function (start, end, color) {
-    socket.emit('draw', start, end, color);
-  });
+socket.on("draw", function (start, end, color) {
+  whiteboard.draw(start, end, color, false);
+});
 
-})();
+whiteboard.on("draw", function (start, end, color) {
+  socket.emit("draw", start, end, color);
+});
--- 05-Bundlers/homework/server.js
+++ 05-Bundlers/homework/server.js
@@ -1,45 +1,44 @@
-var path = require('path');
+var path = require("path");
 
-var http = require('http');
+var http = require("http");
 var server = http.createServer();
 
-var express = require('express');
+var express = require("express");
 var app = express();
 
-var socketio = require('socket.io');
+var socketio = require("socket.io");
 
-server.on('request', app);
+server.on("request", app);
 
 var io = socketio(server);
 
 var inMemoryDrawHistory = [];
 
-server.listen(1337, function () {
-  console.log('The server is listening on port 1337!');
+server.listen(3001, function () {
+  console.log("The server is listening on port 3001!");
 });
 
-app.use(express.static(path.join(__dirname, 'browser')));
+app.use(express.static(path.join(__dirname, "browser")));
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-io.on('connection', function (socket) {
-  console.log(socket.id, 'connected');
+io.on("connection", function (socket) {
+  console.log(socket.id, "connected");
 
-  socket.emit('load', inMemoryDrawHistory);
+  socket.emit("load", inMemoryDrawHistory);
 
-  socket.on('draw', function (start, end, color) {
+  socket.on("draw", function (start, end, color) {
     inMemoryDrawHistory.push({
       start: start,
       end: end,
-      color: color
+      color: color,
     });
-    socket.broadcast.emit('draw', start, end, color);
+    socket.broadcast.emit("draw", start, end, color);
   });
 
-  socket.on('disconnect', function () {
-    console.log('Goodbye, ', socket.id, ' :(');
+  socket.on("disconnect", function () {
+    console.log("Goodbye, ", socket.id, " :(");
   });
 });
-
